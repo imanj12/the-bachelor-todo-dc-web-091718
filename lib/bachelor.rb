66f8a7b099c6b2481f3c7f7188b40f8558327d70
Iman Jawad <imanj12@gmail.com>
2018-09-09 17:15:00 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,9 @@
 def get_first_name_of_season_winner(data, season)
   
-  
+  data.each do |season, season_attrib|
+    season_attrib.eac do |obj|
+      if obj["status"] == "Winner"
+        obj["name"].
   
 end
 
