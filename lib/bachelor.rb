8ec7218a34b850bf5f5d715895969b716e5124e8
Iman Jawad <imanj12@gmail.com>
2018-09-09 17:33:53 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -53,7 +53,7 @@ def get_average_age_for_season(data, season)
   data.each do |season_num, season_attrib|
     if season_num == season
       season_attrib.each do |obj|
-        total_age += obj["age"].to_i
+        total_age += obj["age"].to_f
         total_contestants += 1
       end
     end
